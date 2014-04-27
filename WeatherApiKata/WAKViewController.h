9f9700faf923f6011110ae97e5b5f41ae449006a
Matt Chowning <matt.chowning@detroitlabs.com>
2014-04-26 22:31:59 -0400

    Initial commit

---
+++ WeatherApiKata/WAKViewController.h
@@ -0,0 +1,13 @@
+//
+//  WAKViewController.h
+//  WeatherApiKata
+//
+//  Created by Matt Chowning on 4/27/14.
+//  Copyright (c) 2014 Detroit Labs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WAKViewController : UIViewController
+
+@end
---
+++ WeatherApiKata/WAKViewController.m
@@ -0,0 +1,38 @@
+//
+//  WAKViewController.m
+//  WeatherApiKata
+//
+//  Created by Matt Chowning on 4/27/14.
+//  Copyright (c) 2014 Detroit Labs. All rights reserved.
+//
+
+#import "WAKViewController.h"
+
+@interface WAKViewController ()
+
+@end
+
+@implementation WAKViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
